cbeceb5089527fe06fb3a015c86c23b30d52edbb
Francesco Greco <rxs1996@gmail.com>
2025-08-12 16:38:50 +0200

    text updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,13 +11,20 @@ export default function About() {
                     <div className="col-md-7 text-primary-custom">
                         <h2 className="fw-bold text-primary-custom">Chi sono</h2>
                         <h5>
-                            Sono Ruben Rausa, fisioterapista con oltre 10 anni di esperienza nel trattamento di problematiche muscolo-scheletriche.
+                            Sono Ruben Rausa, fisioterapista con oltre 10 anni di esperienza nel trattamento di
+                            problematiche muscolo-scheletriche.
                         </h5>
                         <h5>
-                            Il mio obiettivo è aiutare le persone a muoversi meglio, vivere con meno dolore e recuperare la propria autonomia, nel modo più sicuro e rispettoso possibile. Le esperienze di lavoro con atleti di alto livello nel mondo del tennis e dell’hockey mi hanno arricchito professionalmente e mi hanno insegnato quanto sia importante un approccio preciso, individuale e orientato al risultato.
+                            Il mio obiettivo è aiutare le persone a muoversi
+                            meglio, vivere con meno dolore e recuperare la propria autonomia, nel modo più
+                            sicuro e rispettoso possibile. Le esperienze di lavoro con atleti di alto livello nel
+                            mondo del tennis e dell’hockey mi hanno arricchito professionalmente e mi hanno
+                            insegnato quanto sia importante un approccio preciso, individuale e orientato al
+                            risultato.
                         </h5>
                         <h5>
-                            Oggi metto queste competenze al servizio di sportivi e pazienti di ogni età, trattando un’ampia gamma di disturbi muscolo-scheletrici.
+                            Oggi metto queste competenze al servizio di sportivi e pazienti di ogni età, trattando
+                            un’ampia gamma di disturbi muscolo-scheletrici.
                         </h5>
                         <ul className="list-inline">
                             <li className="list-inline-item badge bg-light text-dark border">Laurea in Fisioterapia</li>
